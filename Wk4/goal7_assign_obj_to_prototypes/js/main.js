1aa6134455efd9f7c894975d19cacc5a5252fc25
Kimerlee Holcom <kaholcom@fullsail.edu>
2015-01-27 17:14:34 -0500

    if statement catch duplicate names
    if statement to make sure no duplicates, also inside calls the
    populateHTML( ) if there are no duplicates. If there are, then the for
    loop i variable is decreased by one as is the row number.

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -25,5 +25,15 @@
 
         //add person variable (the new Person object) to the people array
         people.push(person);                //use push method to add person to people array
+        if(personName != row1Name.innerHTML && personName != row2Name.innerHTML && personName != row3Name.innerHTML){
+            //if they are not the same, call the populateHTML function twice
+            populateHTML(personName, 'r' + rowNum + 'c1');  //once to enter the name into the HTML
+            populateHTML(person.jobs, 'r' + rowNum + 'c2'); //the second time to enter the job of the person
+
+            //if the names do match one of the 3 rows, then subtract one from the incrementing variable for the loop, and one from the rowNum
+        }else{
+            i--;                    //decreasing var i by 1
+            rowNum--;               //decreasing rowNum by 1
+        };
 
     };
