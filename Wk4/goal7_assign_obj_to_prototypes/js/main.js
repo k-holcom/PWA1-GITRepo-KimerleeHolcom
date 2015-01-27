5ffbae3c9967bc24faefab5f1d86a4f5bea60054
Kimerlee Holcom <kaholcom@fullsail.edu>
2015-01-27 17:07:05 -0500

    add files to GitHub
    added files to my gitHub to get started.

---
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Kimerlee Holcom
+ * Date: January 27, 2015
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
---
+++ Wk4/goal7_assign_obj_to_prototypes/js/person_blueprint.js
@@ -0,0 +1,8 @@
+/**
+ * Name: Kimerlee Holcom
+ * Date: January 27, 2015
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+};
