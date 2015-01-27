9ac582233962c49eaa26ab8807356625cafeb1aa
Kimerlee Holcom <kaholcom@fullsail.edu>
2015-01-27 17:11:28 -0500

    main.js variables
    created self executing function and the global variables

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -5,3 +5,12 @@
  * Class: PWA
  * Goal: Goal7
  */
+(function(){//create self executing function
+
+    //create and define global variables (names, people, rowNum, and 3 variables for the name columns of each row
+    var names = ['Kim', 'Sara', 'Ty', 'Dan', 'Tina'],   //names array created
+        people = [],                                    //people array created
+        rowNum = 1,                                     //initiates row number to 1
+        row1Name = document.getElementById('r1c1'),     //points to column 1 in row 1
+        row2Name = document.getElementById('r2c1'),     //points to column 1 in row 2
+        row3Name = document.getElementById('r3c1');     //points to column 1 in row 3
