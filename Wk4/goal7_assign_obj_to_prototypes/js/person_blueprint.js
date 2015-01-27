c5ba89021727e4ce9bbf99b4d65a64a6ce94d733
Kimerlee Holcom <kaholcom@fullsail.edu>
2015-01-27 17:09:42 -0500

    Person constructor created
    Created the Person Constructor that is to be used in the main.js but
    resides in the person_blueprint.js file.

--- Wk4/goal7_assign_obj_to_prototypes/js/person_blueprint.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person_blueprint.js
@@ -10,3 +10,18 @@ window.Person = Person;                     //storing the Person object on the w
 
 Person.jobs = ['Teacher', 'Student', 'Actor', 'Pilot']; //create the Person.jobs variable, containing an array.
 Person.actions = ['Eating', 'Sleeping', 'Working'];     //create the Person.actions variable, containing an array.
+
+function Person(name, row) {                             //create the constructor function of Person passing in the name and row, as these are the two "keys" that will be sent from main.js
+
+    this.name = name;                       //assign this.name to the value that is stored in the name parameter
+
+    this.actions = Person.actions[Math.floor(Math.random() * Person.actions.length)];   //to assign ONE value to this.actions, must set it equal to Persons.actions[random number (using the .floor, and .random methods of the Math function.)
+
+    this.jobs = Person.jobs[Math.floor(Math.random() * Person.jobs.length)];            //to assign ONE value to this.jobs, must set it equal to Persons.jobs[random number (using the .floor, and .random methods of the Math function.)
+
+    this.row = row;                         //assign this.row to the value stored in the row parameter
+
+    //print out initial action for each person
+    var actionId = document.getElementById('r' + this.row + 'c3');      //point to the column three in that person's row
+    actionId.innerHTML = this.actions;      //sets the innerHTML to the action stored in this.actions
+};
