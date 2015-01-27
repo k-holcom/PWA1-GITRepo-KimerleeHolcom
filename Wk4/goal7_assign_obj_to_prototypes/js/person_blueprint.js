12b5c0c3dc005c14d8cbc5875416e6e85117d562
Kimerlee Holcom <kaholcom@fullsail.edu>
2015-01-27 17:08:44 -0500

    Person.jobs/actions created in person_blueprint.js
    Created Person.jobs and Person.actions arrays in person_blueprint.js
    file as well as stored Person in the window.

--- Wk4/goal7_assign_obj_to_prototypes/js/person_blueprint.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person_blueprint.js
@@ -5,4 +5,8 @@
  * Class: PWA
  * Goal: Goal7
  */
-};
+
+window.Person = Person;                     //storing the Person object on the window so it can be used by another file
+
+Person.jobs = ['Teacher', 'Student', 'Actor', 'Pilot']; //create the Person.jobs variable, containing an array.
+Person.actions = ['Eating', 'Sleeping', 'Working'];     //create the Person.actions variable, containing an array.
